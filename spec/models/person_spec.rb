ecb88965bf6965ee51ea4c56a1b9c446c2d81b90
Grey Elerson <grey.l.elerson@gmail.com>
2014-10-09 18:42:54 -0500

    fix rspec error on People#index test

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,17 +3,21 @@ require 'rails_helper'
 # this block will wrap all of the tests for Person model
 RSpec.describe Person, :type => :model do
   
+  let(:person) do
+    Person.new(first_name: "Alice", last_name: "Cooper")
+  end
+  
   it 'is valid' do
     expect(Person.new(first_name: "Alice", last_name:"Smith")).to be_valid
   end
   
   it 'is invalid without a first name' do
-    person = Person.new(first_name: nil, last_name: "Smith")
+    person.first_name = nil
     expect(person).not_to be_valid
   end
   
   it 'is invalid without a last name' do
-    person = Person.new(first_name: "Alice", last_name: nil)
+    person.last_name = nil
     expect(person).not_to be_valid
   end
   
