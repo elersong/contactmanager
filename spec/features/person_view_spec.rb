3349fb633ab00baeb1a3d48187ad39ba3ae0d45f
Grey Elerson <grey.l.elerson@gmail.com>
2014-10-10 14:54:55 -0500

    display email address on person show page

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -3,55 +3,74 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 describe 'the person view', type: :feature do
-  
-  let(:person) { Person.create(first_name: "John", last_name: "Adams") }
-  
-  before(:each) do
-    person.phone_numbers.create(number:"555-1234")
-    person.phone_numbers.create(number:"555-5678")
-    visit person_path(person)
-  end
-  
-  it 'shows the phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_content(phone.number)
-    end
-  end
-  
-  it 'has link to add new phone number' do
-    expect(page).to have_link("Add Phone Number", href: new_phone_number_path(person_id: person.id))
-  end
-  
-  it 'adds a new phone number to page' do
-    page.click_link('Add Phone Number')
-    page.fill_in('Number', with: "555-8888")
-    page.click_button('Create Phone number')
-    expect(current_path).to eq(person_path(person))
-    expect(page).to have_content('555-8888')
-  end
-  
-  it 'has links to edit phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_link('edit', href: edit_phone_number_path(phone))
-    end
-  end
-  
-  it 'edits a phone number' do
-    phone = person.phone_numbers.first
-    old_number = phone.number
+  describe "(phone number)" do 
     
-    first(:link, 'edit').click
-    page.fill_in('Number', with: "555-9191")
-    page.click_button('Update Phone number')
-    expect(current_path).to eq(person_path(person))
-    expect(page).to have_content('555-9191')
-    expect(page).to_not have_content(old_number)
+    let(:person) { Person.create(first_name: "John", last_name: "Adams") }
+    
+    before(:each) do
+      person.phone_numbers.create(number:"555-1234")
+      person.phone_numbers.create(number:"555-5678")
+      visit person_path(person)
+    end
+    
+    it 'shows the phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_content(phone.number)
+      end
+    end
+    
+    it 'has link to add new phone number' do
+      expect(page).to have_link("Add Phone Number", href: new_phone_number_path(person_id: person.id))
+    end
+    
+    it 'adds a new phone number to page' do
+      page.click_link('Add Phone Number')
+      page.fill_in('Number', with: "555-8888")
+      page.click_button('Create Phone number')
+      expect(current_path).to eq(person_path(person))
+      expect(page).to have_content('555-8888')
+    end
+    
+    it 'has links to edit phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+      end
+    end
+    
+    it 'edits a phone number' do
+      phone = person.phone_numbers.first
+      old_number = phone.number
+      
+      first(:link, 'edit').click
+      page.fill_in('Number', with: "555-9191")
+      page.click_button('Update Phone number')
+      expect(current_path).to eq(person_path(person))
+      expect(page).to have_content('555-9191')
+      expect(page).to_not have_content(old_number)
+    end
+    
+    it 'has links to delete phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_link('delete', href: phone_number_path(phone))
+      end
+    end
   end
   
-  it 'has links to delete phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_link('delete', href: phone_number_path(phone))
+  describe '(email address)' do
+    
+    let(:person) { Person.create(first_name: "John", last_name: "Adams") }
+    
+    before(:each) do
+      person.email_addresses.create(address: "sample@sample.org", person_id: person.id)
+      person.email_addresses.create(address:"this@that.net", person_id: person.id)
+      visit person_path(person)
     end
+    
+    it 'organizes email_addresses into <li> elements on the person"s show page' do
+      email = person.email_addresses.last
+      expect(page).to have_selector('li', text: email.address)
+    end
+    
   end
   
 end
