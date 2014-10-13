3729e9b0493a10e38d8a647d76a36a4aba3f7ddf
Grey Elerson <grey.l.elerson@gmail.com>
2014-10-12 21:16:29 -0500

    finish all polymorphic association tests for company tracking

--- spec/features/company_view_spec.rb
+++ spec/features/company_view_spec.rb
@@ -3,6 +3,7 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 describe 'the company view', type: :feature do 
+ describe '(phone number)' do
     
     let(:company) { Company.create(name: "The Testing View Corporation") }
     
@@ -53,5 +54,59 @@ describe 'the company view', type: :feature do
         expect(page).to have_link('delete', href: phone_number_path(phone))
       end
     end
-  
+ end
+ 
+ context '(email address)' do
+    
+    let(:company) { Company.create(name: "The Testing View Corporation") }
+    
+    before(:each) do
+      company.email_addresses.create(address: "company_view_spec@test.com")
+      company.email_addresses.create(address: "again_company_view_spec2@test.com")
+      visit company_path(company)
+    end
+    
+    it 'displays that companys email addresses' do
+      company.email_addresses.each do |email|
+        expect(page).to have_content(email.address)
+      end
+    end
+    
+    it 'has link to add new email address' do
+      expect(page).to have_link("Add Email Address", href: new_email_address_path(contact_id: company.id, contact_type: "Company"))
+    end
+    
+    it 'adds a new email address to page' do
+      page.click_link('Add Email Address')
+      page.fill_in('Address', with: "company_view_spec@test3.com")
+      page.click_button('Create Email address')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('company_view_spec@test3.com')
+    end
+    
+    it 'has links to edit email addresses' do
+      company.email_addresses.each do |email|
+        expect(page).to have_link('edit', href: edit_email_address_path(email))
+      end
+    end
+    
+    it 'edits an email address' do
+      email = company.email_addresses.first
+      old_email = email.address
+      
+      first(:link, 'edit').click
+      page.fill_in('Address', with: "edited email address string")
+      page.click_button('Update Email address')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content("edited email address string")
+      expect(page).to_not have_content(old_email)
+    end
+    
+    it 'has links to delete email addresses' do
+      company.email_addresses.each do |email|
+        expect(page).to have_link('delete', href: email_address_path(email))
+      end
+    end
+ end
+ 
 end
